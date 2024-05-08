ce00f8969b2a379b7849fc53a5a32b74713453c1
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2024-05-08 07:37:06 +0530

    Add minor changes

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ if __name__ == '__main__':
     while True:
         engine = pyttsx3.init()
 
-        x = input("Enter what you want me to pronounce and press q to exit: ")
+        x = input("Enter what you want me to pronounce and press q to exit: ") 
         if x == "q":
+            engine.say("Have a nice day!")
+            engine.runAndWait()
             break
         engine.say(x)
         engine.runAndWait()
